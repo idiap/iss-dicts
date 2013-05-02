1df74d74e7d61aa726719490693bc62191004048
Phil Garner <Phil.Garner@idiap.ch>
2013-05-02 21:51:01 +0200

    Run the bdlex phonetisuarus build on the grid
    It needs 32G, so 4 slots on a multithreading queue works

--- bdlex/CreatePhSaurus.sh
+++ bdlex/CreatePhSaurus.sh
@@ -10,5 +10,11 @@
 source ../Common.sh
 chdir local
 
+# Submit to a multithreading queue that has 4 threads (with max 8GB
+# each) adding up to 32GB total.  We need the memory, not the threads.
+export USE_GE=1
+export GE_OPTIONS="-l q1dm -pe pe_mth 4"
+
+rm -f job-file
 export IN_DICT=dictionary.txt
 create-phsaurus.sh
