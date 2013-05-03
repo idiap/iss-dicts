09a86b71ab76fcc726b617324125d1437368de1d
Phil Garner <Phil.Garner@idiap.ch>
2013-05-03 14:55:00 +0200

    Add new phonolex variant -de without E~, E~: and O:

--- phonolex/CreateDicts.sh
+++ phonolex/CreateDicts.sh
@@ -15,7 +15,8 @@ sourceDict=$phonolexdict/phonolex
 sourceConv=phonolex.utf8.txt
 tempDict=phonolex.temp
 htkDict=dictionary.txt
-htkDict6=dictionary-6.txt
+htkDict6=dictionary-ext.txt
+htkDictde=dictionary-de.txt
 
 echo "Converting to UTF8"
 iconv --from-code=ISO-8859-1 --to-code=UTF-8 $sourceDict > $sourceConv
@@ -30,3 +31,6 @@ echo "Preparing $htkDict6 (include diphthongs ending in 6)"
 cat $tempDict | sort | uniq > $htkDict6
 echo "Dictionary $htkDict6: done."
 rm $sourceConv $tempDict 
+
+echo "Removing foreign phones for $htkDictde"
+grep -v 'O:\|E~' $htkDict > $htkDictde
