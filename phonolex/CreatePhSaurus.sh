e46cdfb4fb0627dced433ae6a45f9d1b8de3bd3f
Phil Garner <Phil.Garner@idiap.ch>
2013-05-08 17:50:38 +0200

    Create one more German dictionary "-de"

--- phonolex/CreatePhSaurus.sh
+++ phonolex/CreatePhSaurus.sh
@@ -13,9 +13,16 @@ chdir local
 # Submit to a multithreading queue that has 8 threads (with max 8GB
 # each) adding up to 64GB total.  We need the memory, not the threads.
 export USE_GE=1
-export GE_OPTIONS="-l q1dm -l mem_free=64G -pe pe_mth 8"
+export GE_OPTIONS="-l q1wm -pe pe_mth 8"
 
+rm -f job-file
 export IN_DICT=dictionary.txt
 create-phsaurus.sh
-export IN_DICT=dictionary-6.txt
+
+rm -f job-file
+export IN_DICT=dictionary-ext.txt
+create-phsaurus.sh
+
+rm -f job-file
+export IN_DICT=dictionary-de.txt
 create-phsaurus.sh
